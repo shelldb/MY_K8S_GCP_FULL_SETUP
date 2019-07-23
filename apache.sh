b1667d2fefc1a4446fa1244e5c7a46afa4f92a66
Vaib <vaibhavkhandalkar1118@gmail.com>
2019-07-23 20:52:46 +0530

    second version

--- apache.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sudo usermod -G root vaibhavkhandalkar1118
-sudo yum install -y  httpd
-sudo echo "hello vaibbhav" >/opt/temp
--- env_master_setup.sh
+++ env_master_setup.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
+gcloud compute ssh $1 -- "sudo sed -i 's/PasswordAuthentication[[:space:]]no/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
+gcloud compute ssh $1 -- 'sudo systemctl restart sshd'
+gcloud compute ssh $1 -- 'sudo useradd k8test'
+gcloud compute ssh $1 -- "echo -e 'smurf123\nsmurf123' | sudo passwd k8test"
 gcloud compute ssh $1 -- 'sudo usermod -G google-sudoers vaibhavkhandalkar1118'
 gcloud compute ssh $1 -- 'sudo mkdir `pwd`/setup_files'
 gcloud compute ssh $1 -- 'sudo chmod 777 ~/setup_files'
 gcloud compute scp `pwd`/k8s_installation.sh $1:~/setup_files
 gcloud compute scp `pwd`/k8_master_install.sh $1:~/setup_files
+gcloud compute scp `pwd`/helm.sh $1:~/setup_files
 gcloud compute ssh $1 -- 'cd  ~/setup_files && sudo sh k8s_installation.sh'
 gcloud compute ssh $1 -- 'cd  ~/setup_files && sudo sh k8_master_install.sh'
+gcloud compute ssh $1 -- 'cd  ~/setup_files && sudo sh helm.sh'
